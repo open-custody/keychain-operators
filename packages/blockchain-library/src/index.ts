8a8dda3231829e274c3c8b6e5fb2e4718af41e64
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2024-03-20 12:36:31 +0300

    Add wardenprotocol submodule

--- packages/blockchain-library/src/index.ts
+++ packages/blockchain-library/src/index.ts
@@ -1,3 +1,8 @@
+import { MissingWalletError } from '@warden/wardenprotocol-client/dist';
+
 export function bolerplate(): string {
+  console.log(MissingWalletError.message);
+
   return 'hello!';
 }
+bolerplate();
