1307fcd24235665eaec42020fd639cc08d698ede
Maks Nabokov <max.nab@equilibrium.io>
2024-03-21 13:17:49 +0300

    add getVault method

--- packages/fordefi-library/src/index.ts
+++ packages/fordefi-library/src/index.ts
@@ -21,4 +21,15 @@ export class FordefiSerice {
     const response = await axios.post(url, createVaultParams, { headers });
     return response.data;
   }
+
+  async getVault(id: string): Promise<Vault> {
+    const url = new URL('vaults', this.configuration.fordefiAPIEndpoint).toString();
+    const headers = {
+      'Content-type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${this.configuration.accessToken}`,
+    };
+    const response = await axios.post(url, { id }, { headers });
+    return response.data;
+  }
 }
