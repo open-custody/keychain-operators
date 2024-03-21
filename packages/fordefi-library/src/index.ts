364013fe597c50c3fafb5661b1648bfc58ae883e
Maks Nabokov <max.nab@equilibrium.io>
2024-03-21 12:10:25 +0300

    create vault method

--- packages/fordefi-library/src/index.ts
+++ packages/fordefi-library/src/index.ts
@@ -1,3 +1,22 @@
-export function bolerplate(): string {
-  return 'hello!';
+import axios from 'axios';
+
+import { Vault } from './types/vault';
+import { CreateVaultParams } from './types/vault.createParams';
+
+export class FordefiClient {
+  constructor(
+    private fordefiAPIEndpoint: string,
+    private accessToken: string,
+  ) {}
+
+  async createVault(createVaultParams: CreateVaultParams): Promise<Vault> {
+    const url = new URL('vaults', this.fordefiAPIEndpoint).toString();
+    const headers = {
+      'Content-type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${this.accessToken}`,
+    };
+    const response = await axios.post(url, createVaultParams, { headers });
+    return response.data;
+  }
 }
