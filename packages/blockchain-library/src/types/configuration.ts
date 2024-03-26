aa896777f8d15f13fe8e8f95619d66f22340ce41
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2024-03-26 11:35:40 +0300

    Add @warden/blockchain-listener app

--- packages/blockchain-library/src/types/configuration.ts
+++ packages/blockchain-library/src/types/configuration.ts
@@ -3,7 +3,5 @@ export interface IWardenConfiguration {
   rpcURL: string;
   prefix: string;
   pollingIntervalMsec: number;
-  keychainId: string;
   signerMnemonic: string;
-  chainPrefix: string;
 }
