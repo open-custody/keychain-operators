1bf6cfce61ac8b99a0aba177a22adf72ea3a987c
Maks Nabokov <max.nab@equilibrium.io>
2024-03-28 17:55:12 +0300

    add body validation

--- packages/webhooks/src/main.ts
+++ packages/webhooks/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -5,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log('Started at', process.env.WEBHOOK_PORT);
   const app = await NestFactory.create(AppModule, { rawBody: true });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.WEBHOOK_PORT);
 }
 
--- packages/webhooks/src/models/fordefi.webhook.event.ts
+++ packages/webhooks/src/models/fordefi.webhook.event.ts
@@ -1,3 +1,34 @@
+import { Type } from 'class-transformer';
+import { IsNotEmpty, Matches, ValidateNested } from 'class-validator';
+
+import { FORDEFI_NOTE_REGEX } from '../validation/regex';
+
+export class WebhookBlackBoxSignatureStatusChangeEvent {
+  /** The unique identifier of the object in the Fordefi platform. */
+  transaction_id: string;
+  /** @deprecated */
+  is_managed_transaction: boolean;
+  /** Indicates whether the transaction was initiated from the Fordefi system itself,
+   * in contrast to incoming transactions (which are, for example, transfers of funds into a vault visible to Fordefi).
+   */
+  direction: string;
+  /** An optional transaction note.
+   * Used by keychain to write creator and request id.
+   */
+  @Matches(FORDEFI_NOTE_REGEX)
+  @IsNotEmpty()
+  note: string;
+  /** automatically_set if the transaction was automatically set as spam by Fordefi,
+   * manually_set if the transaction was manually set as spam by a user,
+   * and unset if the transaction was not set as spam.
+   */
+  spam_state: string;
+  /** The type of the transaction. */
+  type: string;
+  /** The current state of the message. */
+  state: string;
+}
+
 export class FordefiWebhookEvent {
   webhook_id: string;
   /** The time of the webhook event creation in UTC. */
@@ -8,28 +39,9 @@ export class FordefiWebhookEvent {
   attempt: string;
   /** The time the current event was sent in UTC. */
   sent_at: Date;
+  @ValidateNested()
+  @Type(() => WebhookBlackBoxSignatureStatusChangeEvent)
   event: WebhookBlackBoxSignatureStatusChangeEvent;
   /** The type of the event. */
   event_type: string;
-}
-
-export class WebhookBlackBoxSignatureStatusChangeEvent {
-  /** The unique identifier of the object in the Fordefi platform. */
-  transaction_id: string;
-  /** @deprecated */
-  is_managed_transaction: boolean;
-  /** Indicates whether the transaction was initiated from the Fordefi system itself,
-   * in contrast to incoming transactions (which are, for example, transfers of funds into a vault visible to Fordefi).
-   * */
-  direction: string;
-  /** An optional transaction note. */
-  note?: string;
-  /** automatically_set if the transaction was automatically set as spam by Fordefi,
-   * manually_set if the transaction was manually set as spam by a user,
-   * and unset if the transaction was not set as spam. */
-  spam_state: string;
-  /** The type of the transaction. */
-  type: string;
-  /** The current state of the message. */
-  state: string;
 }
