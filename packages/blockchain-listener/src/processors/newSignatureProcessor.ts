09f499d60b8eb26ddd03a41415a3c74bae41544c
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2024-03-28 18:14:03 +0300

    Add signing request message polling

---
+++ packages/blockchain-listener/src/processors/newSignatureProcessor.ts
@@ -0,0 +1,34 @@
+import { INewSignatureRequest } from '@warden/blockchain-library/dist/types/newKeyRequest copy';
+import { INewSignatureRequestMessage, KeyProvider, MessageBrokerProducer } from '@warden/message-broker-library';
+
+import { Processor } from './processor';
+
+export class NewSignatureProcessor extends Processor<INewSignatureRequest> {
+  constructor(
+    generator: AsyncGenerator<INewSignatureRequest>,
+    private producer: MessageBrokerProducer,
+    private provider: KeyProvider,
+  ) {
+    super(generator);
+  }
+
+  async handle(data: INewSignatureRequest): Promise<boolean> {
+    try {
+      console.log(`New Signature request: ${data.id}`);
+
+      return this.producer.publish<INewSignatureRequestMessage>({
+        provider: this.provider,
+        creator: data.creator,
+        keyId: data.keyId,
+        keychainId: data.keychainId,
+        requestId: data.id,
+        keyType: data.keyType,
+        signingData: data.signingData,
+      });
+    } catch (error) {
+      console.error(error);
+
+      return false;
+    }
+  }
+}
