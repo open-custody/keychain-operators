e3133b9e6f86687ce6e83c9909e07a22cb85c111
Maks Nabokov <max.nab@equilibrium.io>
2024-03-29 14:36:24 +0300

    no return for /fordefi/signature

--- packages/webhooks/src/controllers/fordefi.controller.ts
+++ packages/webhooks/src/controllers/fordefi.controller.ts
@@ -13,19 +13,17 @@ export class AppController {
 
   @Post('/fordefi/signature')
   @UseGuards(SignatureGuard)
-  handleSignature(@Body() fordefiWebhookEvent: FordefiWebhookEvent) {
+  async handleSignature(@Body() fordefiWebhookEvent: FordefiWebhookEvent) {
     const fordefiSignatureState = fordefiWebhookEvent.event.state;
     if (fordefiSignatureState === ('completed' || 'aborted')) {
       const [_, creator, requestId] = fordefiWebhookEvent.event.note.match(FORDEFI_NOTE_REGEX);
 
-      return this.producerService.produce<IFulfilSignatureRequestMessage>({
+      await this.producerService.produce<IFulfilSignatureRequestMessage>({
         creator,
         requestId: parseInt(requestId),
         status: fordefiSignatureState === 'completed' ? FulfilmentStatus.Fulfil : FulfilmentStatus.Reject,
         keyProviderRequestId: fordefiWebhookEvent.event.transaction_id,
       });
     }
-
-    return false;
   }
 }
