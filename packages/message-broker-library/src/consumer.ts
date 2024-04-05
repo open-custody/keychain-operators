353371d1cee5f67c43d9942f14b76d99b4aca4c1
Maks Nabokov <max.nab@equilibrium.io>
2024-04-05 16:38:28 +0300

    message-broker library strict checks

--- packages/message-broker-library/src/consumer.ts
+++ packages/message-broker-library/src/consumer.ts
@@ -2,18 +2,21 @@ import { MessageBroker } from './broker';
 import { IMessageBrokerConsumerConfiguration } from './types/configuration';
 
 export class MessageBrokerConsumer extends MessageBroker {
-  async consume<T>(configuration: IMessageBrokerConsumerConfiguration, handle: (message: T) => Promise<boolean>) {
+  async consume<T>(
+    configuration: IMessageBrokerConsumerConfiguration,
+    handle: (message: T) => Promise<boolean | undefined>,
+  ) {
     await this.channel.prefetch(configuration.prefetch);
 
     await this.channel.consume(
       this.configuration.queue,
       (message) => {
-        const obj = JSON.parse(message.content.toString());
+        const obj = JSON.parse(message!.content.toString());
 
         handle(obj)
           .then((handled) => {
-            if (handled) this.channel.ack(message);
-            else this.channel.reject(message, false);
+            if (handled) this.channel.ack(message!);
+            else this.channel.reject(message!, false);
           })
           .catch(console.error);
       },
