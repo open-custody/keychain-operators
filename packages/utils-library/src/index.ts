9e0f0dd995680863d288d20dc1577bd9216a2612
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2024-05-22 15:31:08 +0300

    Fix account sequence mismatch

--- packages/utils-library/src/index.ts
+++ packages/utils-library/src/index.ts
@@ -1,4 +1,4 @@
 export { delay } from './services/async.js';
 export { uuid } from './services/crypto.js';
-export { logInfo, logError } from './logging/console.js';
+export { logInfo, logWarning, logError } from './logging/console.js';
 export { serialize } from './serialization/json.js';
--- packages/utils-library/src/logging/console.ts
+++ packages/utils-library/src/logging/console.ts
@@ -2,6 +2,10 @@ export function logInfo(message: string) {
   console.log(`[INFO at <${new Date().toISOString()}>]: ${message}`);
 }
 
+export function logWarning(message: string) {
+  console.warn(`[WARNING at <${new Date().toISOString()}>]: ${message}`);
+}
+
 export function logError(message: string) {
   console.error(`[ERROR at <${new Date().toISOString()}>]: ${message}`);
 }
